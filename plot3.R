cd2fc2d91cf1b48e0c5255f692888edd47e467b8
Igor Subbotin <moat1981@gmail.com>
2014-05-06 23:43:07 +0400

    plot 3 implemented

---
+++ plot3.R
@@ -0,0 +1,31 @@
+# Exploratory Data Analysis
+# Course Project 1
+# plot3.R
+
+# load data from file
+data <- read.table("./data/household_power_consumption.txt", header = TRUE, sep=";", na.strings="?", 
+                   colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+
+# create single DateTime variable from Date and Time source variables
+data$DateTime <- paste(data$Date, data$Time)
+data$DateTime <- strptime(data$DateTime, "%d/%m/%Y %H:%M:%S", tz = "UTC")
+
+# convert source character Date variable to real Date type
+data$Date = as.Date(data$Date, "%d/%m/%Y")
+
+# filter out data which is only needed
+startDate <- as.Date("2007-02-01", "%Y-%m-%d")
+endDate <- as.Date("2007-02-02", "%Y-%m-%d")
+data = data[data$Date >= startDate & data$Date <= endDate,
+            c("DateTime", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity",
+              "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")]
+
+# draw plot 3
+plot(data$DateTime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(data$DateTime, data$Sub_metering_2, col = "red")
+lines(data$DateTime, data$Sub_metering_3, col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# save plot to "plot3.pdf"
+dev.copy(png, file = "plot3.png", width = 800)
+dev.off()
